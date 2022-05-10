824737d14fe46ec27bc330791680eaac90a5e4df
abhishek <tech.parsai@gmail.com>
2022-05-10 11:24:30 +0530

    Initial commit

---
+++ blogger-util/src/main/java/com/util/MessageUtil.java
@@ -0,0 +1,14 @@
+package com.util;
+
+public class MessageUtil {
+
+
+    public static void setMessage(){
+        System.out.println("message set");
+    }
+
+    public static void getMessage(){
+        System.out.println("got message from Message broker");
+    }
+
+}
---
+++ blogger-web/src/main/java/com/blogger/web/DriverClass.java
@@ -0,0 +1,18 @@
+package com.blogger.web;
+
+import com.util.MessageUtil;
+
+public class DriverClass {
+
+    public static void main(String[] args) {
+
+
+        MessageUtil.getMessage();
+
+        MessageUtil.setMessage();
+
+
+
+
+    }
+}
